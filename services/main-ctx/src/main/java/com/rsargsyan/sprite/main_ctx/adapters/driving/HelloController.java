39673dcb53c638664ae214454e69ed043aa0c8d4
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2025-11-10 17:50:40 +0100

    restructured adapters folder

--- services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/adapters/driving/HelloController.java
+++ services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/adapters/driving/HelloController.java
@@ -1,4 +1,4 @@
-package com.rsargsyan.sprite.main_ctx.adapters;
+package com.rsargsyan.sprite.main_ctx.adapters.driving;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
