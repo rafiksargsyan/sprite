3c88d23b1c4e1cab5dcc38b2c8413f228ba68f80
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2025-11-10 14:47:48 +0100

    main context init

---
+++ services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/MainCtxApplication.java
@@ -0,0 +1,13 @@
+package com.rsargsyan.sprite.main_ctx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainCtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainCtxApplication.class, args);
+	}
+
+}
