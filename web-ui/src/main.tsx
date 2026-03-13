1f1875fd6b20d36d38584f6cfc17516454faf8f6
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2026-03-13 15:08:08 +0400

    initialized web-ui with material ui

--- web-ui/src/main.tsx
+++ web-ui/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { StrictMode } from 'react'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light', 
+    primary: {
+      main: '#4f46e5',
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
