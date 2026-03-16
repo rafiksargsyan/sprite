be73aa23c0b809407150b67ab6c7a0015be9803e
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2026-03-16 19:43:21 +0400

    Minor dto fix

--- services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/core/app/dto/JpgThumbnailConfigResponse.java
+++ services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/core/app/dto/JpgThumbnailConfigResponse.java
@@ -1,9 +1,12 @@
 package com.rsargsyan.sprite.main_ctx.core.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record JpgThumbnailConfigResponse(int resolution, SpriteSizeResponse spriteSize, int quality, int interval)
     implements ThumbnailConfigResponse {
 
   @Override
+  @JsonProperty
   public String format() {
     return "jpg";
   }
--- services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/core/app/dto/WebpThumbnailConfigResponse.java
+++ services/main-ctx/src/main/java/com/rsargsyan/sprite/main_ctx/core/app/dto/WebpThumbnailConfigResponse.java
@@ -1,9 +1,12 @@
 package com.rsargsyan.sprite.main_ctx.core.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record WebpThumbnailConfigResponse(int resolution, SpriteSizeResponse spriteSize, int quality, int method, boolean lossless, int interval)
     implements ThumbnailConfigResponse {
 
   @Override
+  @JsonProperty
   public String format() {
     return "webp";
   }
